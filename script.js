35f024524b6b950fc90fe1e317d9f40b5a56801f
Otávio Miranda <107643196+hotavio10@users.noreply.github.com>
2022-09-28 22:57:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const nav = document.getElementsById("nav");
+const nav = document.getElementById("nav");
 
 for(const link of nav.getElementsByTagName("a")) {
-    link.onmousemove = e => {
-        const rect = link.getBoundingClientRect(),
-        img = link.querySelector("img2");
-      
-      img.style.left = `${e.clientX - rect.left}px`;
-      img.style.top = `${e.clientY - rect.top}px`;
-    }
+  link.onmousemove = e => {
+    const rect = link.getBoundingClientRect(),
+          img = link.querySelector("img");
+
+    img.style.left = `${e.clientX = rect.left}px`;
+    img.style.top = `${e.clientY = rect.top}px`;
+  }
 }
 
 window.onmousemove = e => {
   const percent = e.clientY / window.innerHeight;
-  
+
   nav.animate({
     transform: `translateY(${percent 
       * nav.offsetHeight * -1}px)`
-  }, {
-    fill: "forwards",
-    duration: 4000
-  })
-}
+    }, {
+      fill: "forwards",
+      duration: 4000
+    })
+    }
